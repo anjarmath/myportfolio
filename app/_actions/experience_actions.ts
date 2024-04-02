82d3d1e9b174428dae90e4677a4bf530ac6a50fc
anjarmath <anjarmath@gmail.com>
2024-04-02 23:21:39 +0700

    fix: dynamic rendering on fetch data

--- app/_actions/experience_actions.ts
+++ app/_actions/experience_actions.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import { getXataClient } from "@/xata";
+import { unstable_noStore as noStore } from "next/cache";
 
 const xataClient = getXataClient();
 
 export const getExperiences = async () => {
+  noStore();
   // fetch experiences
   const resExperiences = await xataClient.db.experience.getAll();
   const experiences = resExperiences.sort((a, b) => b.index - a.index);
